36486f7d1de4e25dae8d00391fc6be1c9195cd85
Daniel <davilareko@gmail.com>
2021-10-12 16:51:18 +0200

    Created example install script for package devs

---
+++ example-package-install.js
@@ -0,0 +1,11 @@
+// Every package needs to contain three arguments, otherwise it will be rejected.
+// `args` are arguments passed while installing/initializing package.
+// `chan` is a channel object collected when package is initialized.
+// `cli` is main bot client object. If the package contains something that would try to token-grab the bot, APT will automatically reject it from installation. 
+exports.Init = function (args, chan, cli) {
+    cli.on("message", (message) => {
+        if (message.content.startsWith("$hi")) {
+            message.channel.send("Hello!");
+        }
+    });
+};
