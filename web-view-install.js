0808dc00fea5644e7d2bad5f915163ec80a8f89c
Daniel <davilareko@gmail.com>
2021-10-22 19:08:36 +0200

    Add testing branch for testing new packages

---
+++ web-view-install.js
@@ -0,0 +1,22 @@
+exports.Init = function (args, chan, cli) {
+    let ls = null;
+    args = message.content.substring(message.content.indexOf(" ") + 1).split(" ");
+    ls = spawn("mpm", ["install", "puppeteer"]);
+
+    ls.on("close", code => {
+        cli.on("message", (message) => {
+            if (message.content.startsWith("$wv")) {
+                const puppeteer = require('puppeteer');
+
+                (async () => {
+                    const browser = await puppeteer.launch();
+                    const page = await browser.newPage();
+                    await page.goto('https://example.com');
+                    await page.screenshot({ path: 'example.png' });
+
+                    await browser.close();
+                })();
+            }
+        });
+    });
+};
