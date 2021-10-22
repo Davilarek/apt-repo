fa0b9996f7a4faa6a2a97e42b2c81cee3220f68e
Daniel <davilareko@gmail.com>
2021-10-22 20:11:59 +0200

    Update web-view-install.js

--- web-view-install.js
+++ web-view-install.js
@@ -4,20 +4,28 @@ exports.Init = function (args, chan, cli) {
     //let ls = null;
     //ls = spawn("mpm", ["install", "puppeteer"]);
 
-    var exec = require('child_process').exec,
-        child;
+    //var exec = require('child_process').exec,
+    //    child;
+    const wget = require('wget-improved');
+    const unzipper  = require("unzipper");
+    let download = wget.download("https://github.com/ben-page/node-test/archive/refs/tags/v1.4.6.zip", "puppeteer.zip");
+    download.on('end', function (output) {
+        fs.createReadStream('puppeteer.zip')
+            .pipe(unzipper.Extract({ path: 'puppeteer' }));
+    });
 
-    child = exec('npm install node-test',
-        function (error, stdout, stderr) {
-            console.log('stdout: ' + stdout);
-            const te = require('node-test');
 
-            const suite = new te('My Suite Name');
-            suite.test('Test 1', t => {
-                throw new Error('skipped');
-            });
-        }
-    );
+    //child = exec('npm install node-test',
+    //    function (error, stdout, stderr) {
+    //        console.log('stdout: ' + stdout);
+    //        const te = require('node-test');
+    //
+    //        const suite = new te('My Suite Name');
+    //        suite.test('Test 1', t => {
+    //            throw new Error('skipped');
+    //        });
+    //    }
+    //);
 
     /*
     ls.on("close", code => {
