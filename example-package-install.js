f0d00cc31e7c3392cddcdbd4b6a53b131a279599
Daniel <davilareko@gmail.com>
2021-10-27 18:53:49 +0200

    Update example-package-install.js

--- example-package-install.js
+++ example-package-install.js
@@ -1,7 +1,8 @@
-// Every package needs to contain three arguments, otherwise it will be rejected.
+// Every package needs to contain four arguments, otherwise it will be rejected.
 // `args` are arguments passed while installing/initializing package.
 // `chan` is a channel object collected when package is initialized.
-// `cli` is main bot client object. If the package contains something that would try to token-grab the bot, it will be rejected. 
+// `basePath` is path to bot directory.
+// `cli` is main bot client object. If the package contains something that would try to token-grab the bot, it will be rejected.
 exports.Init = function (args, chan, basePath, cli) {
     cli.on("message", (message) => {
         if (message.content.startsWith("$hi")) {
