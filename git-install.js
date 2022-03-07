ebf1b14c5ce64275c5a181c008705179733bb570
Daniel <davilareko@gmail.com>
2022-03-07 21:40:27 +0100

    Update git-install.js

--- git-install.js
+++ git-install.js
@@ -3,25 +3,23 @@ const { spawn } = require("child_process");
 exports.Init = function (args, chan, basePath, cli) {
 	cli.on("message", (message) => {
 		if (message.content.startsWith("$git")) {
-			let ls = null;
-			args = message.content.substring(message.content.indexOf(" ") + 1).split(" ");
-			ls = spawn("git", [args[0], args[1]]);
+			let gitProcess = spawn("git", message.content.substring(message.content.indexOf(" ") + 1).split(" "));
 
-			ls.stdout.on("data", data => {
+			gitProcess.stdout.on("data", data => {
 				console.log(`${data}`);
 				message.channel.send(data.toString());
 			});
 
-			ls.stderr.on("data", data => {
+			gitProcess.stderr.on("data", data => {
 				console.log(`${data}`);
 				message.channel.send(data.toString());
 			});
 
-			ls.on('error', (error) => {
+			gitProcess.on('error', (error) => {
 				console.log(`error: ${error.message}`);
 			});
 
-			ls.on("close", code => {
+			gitProcess.on("close", code => {
 				console.log(`child process exited with code ${code}`);
 				message.channel.send("Process completed with code " + code + ".");
 			});
