e6fc2a5929d8fa95c5ffa341dd8e515dcc1a798d
Davilarek <62715937+Davilarek@users.noreply.github.com>
2022-05-09 15:35:19 +0200

    Update text-edit-install.js

--- text-edit-install.js
+++ text-edit-install.js
@@ -1,4 +1,5 @@
 exports.Init = function (args, chan, basePath, cli) {
+    cli.cmdList["edit"] = "edit a file";
     cli.on("message", (message) => {
         if (message.content.startsWith("$edit")) {
             const fs = require('fs');
@@ -63,4 +64,4 @@ exports.Init = function (args, chan, basePath, cli) {
     });
 };
 
-exports.Version = 2.1;
+exports.Version = 2.3;
