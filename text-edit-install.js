93b5e330f28f7c520e06bcb5ad8dd833e3585cdb
Daniel <davilareko@gmail.com>
2022-06-26 23:26:00 +0200

    Update text-edit-install.js

--- text-edit-install.js
+++ text-edit-install.js
@@ -10,61 +10,67 @@ exports.Init = function (args, chan, basePath, cli) {
             }
             else {
                 if (fs.existsSync(message.content.substring(message.content.indexOf(" ") + 1))) {
-                    let filter = m => m.author.id === message.author.id;
-                    let filename = message.content.substring(message.content.indexOf(" ") + 1);
-                    // prevent user from executing commands while in text edit mode
-                    // cli.enableStdin = false;
-                    message.channel.send(`Type anything or type \"\`cancel\`\" to cancel. Waiting for data...`).then(() => {
-                        message.channel.awaitMessages(filter, {
-                            max: 1,
-                            time: 30000,
-                            errors: ['time']
-                        })
-                            .then(message => {
-                                message = message.first()
-                                if (message.content.toUpperCase() == 'CANCEL' || message.content.toUpperCase() == 'C') {
-                                    message.channel.send(`Terminated`)
-                                } else {
-                                    /**
-                                     * @type {string}
-                                     */
-                                    var data = message.content.toString();
-                                    var dataClear = data;
-                                    var dataLines = data.split("\n")
-                                    if (dataLines[0].startsWith("```") && dataLines[dataLines.length - 1].startsWith("```")) {
-                                        //console.log(data.replace(/(\r)+/gm, "\\r").replace(/(\n)+/gm, "\\n"))
-                                        //dataClear = "\0";
-                                        //console.log(data.split(/(\n)+/gm));
-                                        // for (let i = 0; i < data.split(/(\n)+/gm).length; i++) {
-                                        //     //console.log(i);
-                                        //     //console.log(data.split(/(\n)+/gm)[i]);
-                                        //     if (i == 0) { continue; }
-                                        //     if (i == (data.split(/(\n)+/gm).length-1)) { continue; }
-                                        //     dataClear += data.split(/(\n)+/gm)[i];
-                                        // }
-
-                                        var lines = data.split('\n');
-                                        lines.splice(0, 1);
-                                        lines.splice(lines.length - 1, 1);
-                                        dataClear = lines.join('\n');
-                                    }
-                                    console.log(dataClear);
-                                    fs.writeFileSync(filename, dataClear);
-                                }
-                            })
-                            .catch(collected => {
-                                console.log(collected);
-                                message.channel.send('Timeout');
-                            });
-                    })
-                    // cli.enableStdin = true;
+                    editFile(message);
                 }
                 else {
-                    message.channel.send("Error: target file doesn't exist.");
+                    message.channel.send("Target file doesn't exist. Creating one for you...");
+                    cli.executeCommand(cli.fakeMessageCreator("$touch " + message.content.substring(message.content.indexOf(" ") + 1)))
+                    editFile(message);
                 }
             }
         }
     });
 };
+
+function editFile(filename, message) {
+    let filter = m => m.author.id === message.author.id;
+    let filename = message.content.substring(message.content.indexOf(" ") + 1);
+    // prevent user from executing commands while in text edit mode
+    // cli.enableStdin = false;
+    message.channel.send(`Type anything or type \"\`cancel\`\" to cancel. Waiting for data...`).then(() => {
+        message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 30000,
+            errors: ['time']
+        })
+            .then(message => {
+                message = message.first()
+                if (message.content.toUpperCase() == 'CANCEL' || message.content.toUpperCase() == 'C') {
+                    message.channel.send(`Terminated`)
+                } else {
+                    /**
+                     * @type {string}
+                     */
+                    var data = message.content.toString();
+                    var dataClear = data;
+                    var dataLines = data.split("\n")
+                    if (dataLines[0].startsWith("```") && dataLines[dataLines.length - 1].startsWith("```")) {
+                        //console.log(data.replace(/(\r)+/gm, "\\r").replace(/(\n)+/gm, "\\n"))
+                        //dataClear = "\0";
+                        //console.log(data.split(/(\n)+/gm));
+                        // for (let i = 0; i < data.split(/(\n)+/gm).length; i++) {
+                        //     //console.log(i);
+                        //     //console.log(data.split(/(\n)+/gm)[i]);
+                        //     if (i == 0) { continue; }
+                        //     if (i == (data.split(/(\n)+/gm).length-1)) { continue; }
+                        //     dataClear += data.split(/(\n)+/gm)[i];
+                        // }
+
+                        var lines = data.split('\n');
+                        lines.splice(0, 1);
+                        lines.splice(lines.length - 1, 1);
+                        dataClear = lines.join('\n');
+                    }
+                    console.log(dataClear);
+                    fs.writeFileSync(filename, dataClear);
+                }
+            })
+            .catch(collected => {
+                console.log(collected);
+                message.channel.send('Timeout');
+            });
+    })
+    // cli.enableStdin = true;
+}
 
 exports.Version = 2.6;
